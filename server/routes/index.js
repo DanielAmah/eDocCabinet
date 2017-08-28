03aa12044e540991df68c51bd959d12b05afb802
DanielAmah <amahdanieljack@gmail.com>
2017-08-28 23:58:35 +0100

    chore(fix): fix route naming convention

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,13 @@
 import path from 'path';
 
 import authentication from '../middleware/authentication';
-import DocumentController from '../controllers/DocumentController';
-import RoleController from '../controllers/RoleController';
-import UserController from '../controllers/UserController';
-import SearchController from '../controllers/SearchController';
-import AuthController from '../controllers/AuthController';
+import documentController from '../controllers/documentController';
+import roleController from '../controllers/roleController';
+import userController from '../controllers/userController';
+import searchController from '../controllers/searchController';
+import authController from '../controllers/authController';
 
-const Routes = (app) => {
+const routes = (app) => {
   app.post('/', (req, res) =>
     res.status(200).send({
       message: 'Welcome to E-DocCabinet - A document management API'
@@ -19,36 +19,36 @@ const Routes = (app) => {
     })
   );
   // all user routes
-  app.post('/api/v1/users/', UserController.signup);
-  app.post('/api/v1/users/login', AuthController.login);
+  app.post('/api/v1/users/', userController.signup);
+  app.post('/api/v1/users/login', authController.login);
   app.use(authentication.verifyUser);
-  app.get('/api/v1/users/', UserController.listUsers);
-  app.get('/api/v1/users-docs/', UserController.listUsersAndDocuments);
-  app.put('/api/v1/users/:userId', UserController.updateUsers);
-  app.put('/api/v1/users-role/:userId', UserController.updateUsersRole);
-  app.get('/api/v1/users/:userId', UserController.findUsers);
-  app.delete('/api/v1/users/:userId', UserController.deleteUsers);
-  app.get('/api/v1/search/users/', SearchController.searchUsers);
-  app.get('/api/v1/users/:userId/documents/', UserController.findUserDocument);
+  app.get('/api/v1/users/', userController.listUsers);
+  app.get('/api/v1/users-docs/', userController.listUsersAndDocuments);
+  app.put('/api/v1/users/:userId', userController.updateUsers);
+  app.put('/api/v1/users-role/:userId', userController.updateUsersRole);
+  app.get('/api/v1/users/:userId', userController.findUsers);
+  app.delete('/api/v1/users/:userId', userController.deleteUsers);
+  app.get('/api/v1/search/users/', searchController.searchUsers);
+  app.get('/api/v1/users/:userId/documents/', userController.findUserDocument);
 
   // all document routes
-  app.post('/api/v1/documents/', DocumentController.newDocument);
-  app.get('/api/v1/documents', DocumentController.showDocuments);
-  app.put('/api/v1/documents/:documentId', DocumentController.updateDocument);
-  app.get('/api/v1/documents/:documentId', DocumentController.findDocument);
-  app.get('/api/v1/search/documents/', SearchController.searchDocument);
+  app.post('/api/v1/documents/', documentController.newDocument);
+  app.get('/api/v1/documents', documentController.showDocuments);
+  app.put('/api/v1/documents/:documentId', documentController.updateDocument);
+  app.get('/api/v1/documents/:documentId', documentController.findDocument);
+  app.get('/api/v1/search/documents/', searchController.searchDocument);
   app.delete(
     '/api/v1/documents/:documentId',
-    DocumentController.deleteDocument
+    documentController.deleteDocument
   );
 
   // all Role routes
-  app.post('/api/v1/roles/', RoleController.newRole);
-  app.get('/api/v1/roles', RoleController.listRoles);
-  app.get('/api/v1/roles-users/', RoleController.listRolesAndUsers);
+  app.post('/api/v1/roles/', roleController.newRole);
+  app.get('/api/v1/roles', roleController.listRoles);
+  app.get('/api/v1/roles-users/', roleController.listRolesAndUsers);
 
   app.use('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../../public', 'index.html'));
   });
 };
-export default Routes;
+export default routes;
